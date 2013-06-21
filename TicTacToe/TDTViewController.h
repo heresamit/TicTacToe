9e05bd5f0af0b1fe16abdb1263102463638b6c35
Amit Chowdhary <heresamit@gmail.com>
2013-06-21 10:21:51 +0530

    Initial Commit

---
+++ TicTacToe/TDTViewController.h
@@ -0,0 +1,13 @@
+//
+//  TDTViewController.h
+//  TicTacToe
+//
+//  Created by Amit Chowdhary on 21/06/13.
+//  Copyright (c) 2013 Amit Chowdhary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDTViewController : UIViewController
+
+@end
---
+++ TicTacToe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TicTacToe
+//
+//  Created by Amit Chowdhary on 21/06/13.
+//  Copyright (c) 2013 Amit Chowdhary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TDTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TDTAppDelegate class]));
+    }
+}
